3cccc83c7d39c9341a3a03d34a2387b8953dd8f6
josemiguellorenzolara <josemiguel.lorenzolara1501@gmail.com>
2023-02-22 13:17:29 +0100

    Modificando Vistas

---
+++ Proyecto PMDM Swift/InicioApp.swift
@@ -0,0 +1,24 @@
+//
+//  InicioApp.swift
+//  Proyecto PMDM Swift
+//
+//  Created by José Miguel Lorenzo Lara on 22/2/23.
+//
+
+import UIKit
+
+class InicioApp: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBOutlet weak var txtDni: UITextField!
+
+    @IBAction func btnSiguiente(_ sender: UIButton) {
+        
+    }
+}
